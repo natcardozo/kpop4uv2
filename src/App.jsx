d3eaae0420d6d74e4acf8723ac777b8fb0900be0
Nathalia Cardozo <nathalia.cardozo@alura.com.br>
2023-09-23 18:03:34 -0300

    setup and header

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+import Header from "./components/Header"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#364F6B',
+    },
+    secondary: {
+      main: '#F5F5F5',
+    },
+    pink: {
+      main: '#FC5185',
+      light: '#FD759F',
+      dark: '#fB2868',
+      contrastText: '#F5F5F5',
+    },
+    green: {
+      main: '#3FC1C9',
+      light: '#72D2D7',
+      dark: '#2B939A',
+      contrastText: '#F5F5F5',
+    }
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+import { AppBar, Link, Typography} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const styles = {
+    header: {
+        height: "180px", 
+        padding: "14px", 
+        boxSizing: "border-box", 
+        display: "flex", 
+        flexDirection: "row", 
+        justifyContent: "space-around", 
+        alignItems: "center"
+    },
+    link: {
+        fontSize: "22px",
+        textTransform: "uppercase",
+        cursor: "pointer",
+        fontWeight: "700"
+    },
+    welcome: {
+        fontSize: "18px",
+        fontFamily: "Poppins, sans-serif",
+        cursor: "default"
+    }
+}
+
+const Header = () => {
+    return (
+        <AppBar sx={styles.header}>
+            <img src="/images/logo.png" alt="Logo da K-Pop 4 U" />
+            <Link color="secondary" sx={styles.link} underline="hover">Início</Link>
+            <Link color="secondary" sx={styles.link} underline="hover">Categorias</Link>
+            <Link color="secondary" sx={styles.link} underline="hover">Sobre nós</Link>
+            <Link color="secondary" sx={styles.link} underline="hover">Contato</Link>
+            <Typography sx={styles.welcome}>Olá, visitante!</Typography>
+            <PersonIcon fontSize="large" cursor="pointer" />
+            <ShoppingCartIcon fontSize="large" cursor="pointer" />
+        </AppBar>
+    )
+}
+
+export default Header;
